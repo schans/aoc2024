0ef8852be65b01f9b245d4258594e26e31483b18
Bart van der Schans <bart@vanderschans.nl>
2024-12-05 22:41:57 +0100

    Cleanup

--- 05/solve.py
+++ 05/solve.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env pypy3
 
 import fileinput
+from collections import defaultdict
 from functools import cmp_to_key
-
 
 # counters
 T = 0
 T2 = 0
 
-
-B = {}
-A = {}
-O = []
+B = defaultdict(list)
+A = defaultdict(list)
+P = []
 for line in fileinput.input():
     l = line.strip()
     if not l:
@@ -20,61 +19,48 @@ for line in fileinput.input():
     if '|' in l:
         b, a = (int(c) for c in l.split('|'))
 
-        if b in B:
-            B[b].append(a)
-        else:
-            B[b] = [a]
-        if a in A:
-            A[a].append(b)
-        else:
-            A[a] = [b]
+        B[b].append(a)
+        A[a].append(b)
     elif ',' in l:
-        O.append([int(c) for c in l.split(',')])
+        P.append([int(c) for c in l.split(',')])
 
 
-def is_correct(order):
-    for i in range(len(order)):
+def is_ordered(pages):
+    for i in range(len(pages)):
         for j in range(0, i):
-            if order[i] in B and order[j] in B[order[i]]:
+            if pages[j] in B[pages[i]]:
                 return False
-            if order[j] in A and order[i] in A[order[j]]:
+            if pages[i] in A[pages[j]]:
                 return False
-        for j in range(i+1, len(order)):
-            if order[i] in A and order[j] in A[order[i]]:
+        for j in range(i+1, len(pages)):
+            if pages[j] in A[pages[i]]:
                 return False
-            if order[j] in B and order[i] in B[order[j]]:
+            if pages[i] in B[pages[j]]:
                 return False
     return True
 
 
-def compare(item1, item2):
-    # item1 before item2
-    if item1 in B and item2 in B[item1]:
+def compare(page1, page2):
+    # page1 before page2
+    if page2 in B[page1]:
         return -1
-    if item2 in A and item1 in A[item2]:
+    if page1 in A[page2]:
         return -1
 
-    # item2 before item1
-    if item1 in A and item2 in A[item1]:
+    # page2 before page1
+    if page2 in A[page1]:
         return 1
-    if item2 in B and item1 in B[item2]:
+    if page1 in B[page2]:
         return 1
 
     assert False, 'unsorted?'
 
 
-def sort_order(order):
-    if is_correct(order):
-        return order
-    new_order = sorted(order, key=cmp_to_key(compare))
-    return sort_order(new_order)
-
-
-for order in O:
-    if is_correct(order):
-        T += order[len(order)//2]
+for pages in P:
+    if is_ordered(pages):
+        T += pages[len(pages)//2]
     else:
-        new_order = sort_order(order)
-        T2 += new_order[len(new_order)//2]
+        new_pages = sorted(pages, key=cmp_to_key(compare))
+        T2 += new_pages[len(new_pages)//2]
 
 print(f"Tot {T} {T2}")
