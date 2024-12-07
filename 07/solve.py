912ad311c67466ebee72d72d1303f522961bd5cf
Bart van der Schans <bart@vanderschans.nl>
2024-12-07 12:57:25 +0100

    Cleanup

--- 07/solve.py
+++ 07/solve.py
@@ -7,16 +7,6 @@ T = 0
 T2 = 0
 
 
-R = []
-for line in fileinput.input():
-    l = line.strip()
-    if not l:
-        continue
-
-    a, b = l.split(':')
-    R.append([int(a), [int(c) for c in b.split()]])
-
-
 def check(s, v, cc,  cul, p):
     if p == len(v):
         return cul == s
@@ -27,10 +17,18 @@ def check(s, v, cc,  cul, p):
         return check(s, v, cc, cul + v[p], p+1) or check(s, v, cc, cul*v[p], p+1)
 
 
-for s, v in R:
+for line in fileinput.input():
+    l = line.strip()
+    if not l:
+        continue
+
+    s, v = l.split(':')
+    s = int(s)
+    v = [int(c) for c in v.split()]
     if check(s, v, False, v[0], 1):
         T += s
     if check(s, v, True, v[0], 1):
         T2 += s
+
 
 print(f"Tot {T} {T2}")
